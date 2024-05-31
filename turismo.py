25eb7d5deacc177a6da353df4ac2948d911aaa31
mariaabotelho <168577465+mariaabotelho@users.noreply.github.com>
2024-05-31 14:51:33 -0300

    Update turismo.py

--- turismo.py
+++ turismo.py
@@ -8,7 +8,7 @@ def set_style():
         """
         <style>
         .stApp {
-            background: url('https://raw.githubusercontent.com/mariaabotelho/turismo/main/fundo3.jpeg') no-repeat center center fixed;
+            background: url('https://raw.githubusercontent.com/mariaabotelho/turismo/main/fundo2.jpeg') no-repeat center center fixed;
             background-size: cover;
         }
         .css-1d391kg {
